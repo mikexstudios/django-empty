f4519fb77081f7c8ee2beabe072318510dc3bf79
Michael Huynh <mike.huynh@gmail.com>
2010-04-07 17:57:03 -0400

    Rename app now works. It's kind of crude, but oh well.

--- rename_app.sh
+++ rename_app.sh
@@ -12,23 +12,29 @@ cd ${script_path}
 
 #Now go through each of the spots where we had to hardcode app and replace it with
 #our new name. We rename the directories last. 
-#-i means inplace editing.
+#-i means inplace editing. Need to specify an extension that it is saved in. So ''.
+
+#settings.py. NOTE: We hardcode lines here. This is bad, but oh well.
+sed -i '' -e '107s|app|'$1'|' settings.py
+sed -i '' -e 's|app.backends|'$1'.backends|' settings.py
+sed -i '' -e 's|app.helpers|'$1'.helpers|' settings.py
+sed -i '' -e 's|# app|# '$1'|' settings.py
 
 #urls.py
-sed -i "s/app\.urls/$1\.urls/g" urls.py
-sed -i "s/app\.views/$1\.views/g" urls.py
+sed -i '' -e 's|app.urls|'$1'.urls|g' urls.py
+sed -i '' -e 's|app.views|'$1'.views|g' urls.py
 
 #app/urls.py
-sed -i "s/app\.views/$1\.views/g" app/urls.py
+sed -i '' -e 's|app.views|'$1'.views|g' app/urls.py
 
 #app/views.py
-sed -i "s/app\/home/$1\/home/g" app/views.py
+sed -i '' -e 's|app/home|'$1'/home|g' app/views.py
 
 #app/templates/app/home.html. Replace only first line.
-sed -i "1s/app/$1/g" app/templates/app/home.html
+sed -i '' -e '1s|app|'$1'|' app/templates/app/home.html
 
 #Now for the app/templates/django_rpx_plus/*.html files
-sed -i "1s/app/$1/g" app/templates/app/*.html
+sed -i '' -e '1s|app|'$1'|' app/templates/app/*.html
 
 #Now rename our directories
 #app/templates/app/ 
